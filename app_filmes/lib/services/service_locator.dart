2c73f0223248eeab48b8f4fecfbbfb32b902976a
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-21 22:59:20 -0300

    Adiciona a tela de lista de filmes e configura o serviço de localização

---
+++ app_filmes/lib/services/service_locator.dart
@@ -0,0 +1,10 @@
+import 'package:app_filmes/controller/movie_list_controller.dart';
+import 'package:app_filmes/data/models/movie_api.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupGetIt() {
+  getIt.registerLazySingleton<MovieApi>(() => MovieApi());
+  getIt.registerSingleton<MovieListController>(MovieListController());
+}
