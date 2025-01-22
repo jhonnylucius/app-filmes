a76d3d4727ffaef359e2d4f7688b22befd653621
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-22 11:37:52 -0300

    refatora a tela de lista de filmes e ajusta a classe MovieComment

--- app_filmes/lib/data/models/movie_comment.dart
+++ app_filmes/lib/data/models/movie_comment.dart
@@ -1,13 +1,13 @@
 class MovieComment {
-  final int id;
-  final String comment;
-  final DateTime createdAt; // Changed from date to createdAt
-
   MovieComment({
     required this.id,
     required this.comment,
     required this.createdAt,
   });
+
+  final int id;
+  final String comment;
+  final DateTime createdAt; // Changed from date to createdAt
 
   factory MovieComment.fromJson(Map<String, dynamic> json) => MovieComment(
         id: json['id'],
---
+++ app_filmes/lib/screens/movie_list/movie_list_screen.dart
@@ -0,0 +1,55 @@
+import 'package:app_filmes/controller/movie_list_controller.dart';
+import 'package:app_filmes/data/models/movie.dart';
+import 'package:app_filmes/services/service_locator.dart';
+import 'package:flutter/material.dart';
+
+class MovieListScreen extends StatefulWidget {
+  const MovieListScreen({super.key});
+
+  @override
+  State<MovieListScreen> createState() => _MovieListScreenState();
+}
+
+class _MovieListScreenState extends State<MovieListScreen> {
+  final controller = getIt<MovieListController>();
+
+  @override
+  void initState() {
+    controller.init();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Filmes'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+            tooltip: 'Buscar',
+          ),
+        ],
+      ),
+      body: StreamBuilder<List<Movie>>(
+        stream: controller.stream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final movie = snapshot.data![index];
+                return ListTile(
+                  title: Text(movie.name),
+                );
+              },
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
